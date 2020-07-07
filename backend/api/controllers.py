e9d176a7bd650c9bb9c92738c7aaa4061360b296
Karol Lasocki <karolasocki@gmail.com>
2020-07-07 13:42:53 +0200

    Add question endpoint

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -1,4 +1,6 @@
 import logging
+import json
+import random
 from aiohttp import web
 from uuid import UUID
 
@@ -14,3 +16,20 @@ async def add_player(request):
     id: UUID = waiting_room.add_player(nick)
     logging.info("Create player with id %s" % id)
     return web.json_response(dict(id=str(id)))
+
+
+async def get_questions(request):
+    default_number = 3
+    questions_path = "api/data/questions.json"
+
+    data = await request.post()
+    num = data.get("num") if "num" in data else request.rel_url.query.get("num")
+    logging.info(f"Getting {num} questions")
+    try:
+        num = int(num)
+    except:
+        num = default_number
+
+    with open(questions_path, 'r') as f:
+        questions = json.load(f)
+    return web.json_response(random.sample(questions, num))
