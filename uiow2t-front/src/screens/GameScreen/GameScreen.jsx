32856ec4cef1d55928382962e87a3aa7aa110c37
BrtqKr <brtkrason@gmail,com>
2020-07-08 00:42:28 +0200

    initial game screen structure

---
+++ uiow2t-front/src/screens/GameScreen/GameScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Background = styled.div`
+    background-color: 'red';
+`
+
+export default class GameScreen extends React.Component {
+    render() {
+        return (
+            <Background>
+
+            </Background>
+        )
+    }
+}
