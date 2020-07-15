cb657902c2f1189adb2eaa6e3cf304ed3bb274d1
BrtqKr <bartlomiej.krason@swmansion.com>
2020-07-15 21:20:25 +0200

    add basic dragging

--- uiow2t-front/src/components/board/Field.jsx
+++ uiow2t-front/src/components/board/Field.jsx
@@ -1,54 +1,85 @@
-import React from 'react'
-import styled from 'styled-components'
-import { connect } from 'react-redux';
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { TileTypes } from "../dragTypes/TileTypes";
+import { useDrop } from "react-dnd";
 
-
-const mapStateToProps = function (state) {
-    return {
-        currentPosition: state.currentPosition,
-    }
-}
+const mapStateToProps = function(state) {
+  return {
+    currentPosition: state.currentPosition
+  };
+};
 
 const Frame = styled.div`
-    width: 95.5px;
-    height: 60px;
-    background-color: rgba(0,0,255,0.7);
-    margin-right: 5px;
-    margin-bottom: 5px;
-`
+  width: 95.5px;
+  height: 60px;
+  background-color: rgba(0, 0, 255, 0.7);
+  margin-right: 5px;
+  margin-bottom: 5px;
+`;
 const FrameRight = styled.div`
-    width: 95.5px;
-    height: 60px;
-    background-color: rgba(0,0,255,0.7);
-    margin-bottom: 5px;
-`
+  width: 95.5px;
+  height: 60px;
+  background-color: rgba(0, 0, 255, 0.7);
+  margin-bottom: 5px;
+`;
 const FrameBottom = styled.div`
-    width: 95.5px;
-    height: 60px;
-    background-color: rgba(0,0,255,0.7);
-    margin-right: 5px;
-    margin-bottom: 0px;
-`
+  width: 95.5px;
+  height: 60px;
+  background-color: rgba(0, 0, 255, 0.7);
+  margin-right: 5px;
+  margin-bottom: 0px;
+`;
 const FrameBottomRight = styled.div`
-    width: 95.5px;
-    height: 60px;
-    background-color: rgba(0,0,255,0.7);
-    margin-right: 0px;
-    margin-bottom: 0px;
-`
+  width: 95.5px;
+  height: 60px;
+  background-color: rgba(0, 0, 255, 0.7);
+  margin-right: 0px;
+  margin-bottom: 0px;
+`;
 
 function Field({ index, children, dispatch }) {
-    const setCurrentPosition = (newPosition) => dispatch({ type: 'SET_CURRENT_POSITION', newPosition })
+  const [{ isOver }, drop] = useDrop({
+    accept: TileTypes.BENCH_TILE,
+    drop: monitor => {
+      setCurrentPosition([x, y]);
+    },
+    collect: monitor => ({
+      isOver: !!monitor.isOver()
+    })
+  });
+  const setCurrentPosition = newPosition =>
+    dispatch({ type: "SET_CURRENT_POSITION", newPosition });
 
-    const isRight = (index - 9) % 10 === 0
-    const isBottom = (index) >= 50
-    const x = index % 10
-    const y = Math.floor(index / 6)
+  const isRight = (index - 9) % 10 === 0;
+  const isBottom = index >= 50;
+  const x = index % 10;
+  const y = Math.floor(index / 6);
 
-    if (isRight && isBottom) return <FrameBottomRight onClick={() => setCurrentPosition([x, y])}>{children}</FrameBottomRight>
-    else if (isRight) return <FrameRight onClick={() => setCurrentPosition([x, y])}>{children}</FrameRight>
-    else if (isBottom) return <FrameBottom onClick={() => setCurrentPosition([x, y])}>{children}</FrameBottom>
-    else return <Frame onClick={() => setCurrentPosition([x, y])}>{children}</Frame>
+  if (isRight && isBottom)
+    return (
+      <FrameBottomRight ref={drop} onClick={() => setCurrentPosition([x, y])}>
+        {children}
+      </FrameBottomRight>
+    );
+  else if (isRight)
+    return (
+      <FrameRight ref={drop} onClick={() => setCurrentPosition([x, y])}>
+        {children}
+      </FrameRight>
+    );
+  else if (isBottom)
+    return (
+      <FrameBottom ref={drop} onClick={() => setCurrentPosition([x, y])}>
+        {children}
+      </FrameBottom>
+    );
+  else
+    return (
+      <Frame ref={drop} onClick={() => setCurrentPosition([x, y])}>
+        {children}
+      </Frame>
+    );
 }
 
-export default connect(mapStateToProps)(Field)
+export default connect(mapStateToProps)(Field);
--- uiow2t-front/src/components/unitsPanel/UnitBenchTile.jsx
+++ uiow2t-front/src/components/unitsPanel/UnitBenchTile.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { useDrag, DragSource } from "react-dnd";
+import { TileTypes } from "../dragTypes/TileTypes";
 
+const BenchTileSource = {
+  beginDrag(props) {
+    // Return the data describing the dragged item
+    const item = { unit: props.unit };
+    return item;
+  }
+};
 const TileBackground = styled.div`
   background-color: white;
   align-self: flex-end;
@@ -8,12 +17,34 @@ const TileBackground = styled.div`
   width: 80px;
   display: inline-block;
   margin: 0px 4px 5px 4px;
+  cursor: "move";
 `;
 
-export default function UnitBenchTile({ unit }) {
+function collect(connect, monitor) {
+  return {
+    // Call this function inside render()
+    // to let React DnD handle the drag events:
+    connectDragSource: connect.dragSource(),
+    // You can ask the monitor about the current drag state:
+    isDragging: monitor.isDragging()
+  };
+}
+
+function UnitBenchTile({ unit }) {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: TileTypes.BENCH_TILE },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  });
   return unit ? (
-    <TileBackground>{unit.name}</TileBackground>
+    <TileBackground ref={drag}>{unit.name}</TileBackground>
   ) : (
     <TileBackground />
   );
 }
+export default DragSource(
+  TileTypes.BENCH_TILE,
+  BenchTileSource,
+  collect
+)(UnitBenchTile);
