64a8f855ddf30ac5ff464db9881f38e0229571e4
sobolewskidamian <damiansobolewski12@gmail.com>
2020-07-16 11:13:18 +0200

    get if for each unit

--- backend/game/shop/shop.py
+++ backend/game/shop/shop.py
@@ -22,6 +22,7 @@ class Shop:
         with open(self.path_to_units_file, 'r') as units_f:
             self.units_list = self.read_json(units_f)
         self.units_amount = len(self.units_list)
+        self.players_units = []
         self.add_prices()
 
     def read_json(self, file: TextIO) -> List[Dict]:
@@ -49,14 +50,19 @@ class Shop:
     def get_random_units(self, quantity: int, currency: int) -> List[Dict]:
         # If player is too poor to afford anything, return total random
         if currency < 2:
-            return random.choices(self.units_list, quantity)
+            units_to_return = random.choices(self.units_list, quantity)
         else:
             # Else random few random units and few affordable units
-            units_to_return = []
             random_amount = int(floor(quantity * 2 / 3))
-            units_to_return += random.choices(self.units_list, k=random_amount)
+            units_to_return = random.choices(self.units_list, k=random_amount)
             units_to_return += self.get_affordable_units(currency, (quantity - random_amount))
-            return units_to_return
+        return [self._get_unit_with_id(u) for u in units_to_return]
+
+    def _get_unit_with_id(self, unit: Dict) -> Dict:
+        result = dict(unit)
+        result['id'] = len(self.players_units)
+        self.players_units.append(result)
+        return result
 
     def get_affordable_units(self, currency: int, quantity: int) -> List:
         return random.choices([unit for unit in self.units_list if unit['price'] <= currency], k=quantity)
